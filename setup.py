c717a2907b2ac0041152290ece8a757a20dc0297
ParkSeungMin1 <106250439+ParkSeungMin1@users.noreply.github.com>
2023-05-01 18:04:42 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name="crawling_comments",
+  version='0.0.1',
+  description='oss-dev my crawling_comments 0001',
+  author='seungminpark',
+  author_email='1828421@donga.ac.kr',
+  url = "https://github.com/ParkSeungMin1/crawling_comments",
+  download_url="https://github.com/ParkSeungMin1/crawling_comments/archive/refs/tags/v0.0.1.zip",
+  install_requires=['pytest'],
+  long_description='oss-dev crawling_comments python module',
+  long_description_content_type="text/markdown",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
